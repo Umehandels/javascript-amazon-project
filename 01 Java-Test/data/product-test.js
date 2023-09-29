848abde57ee938d387d1d3ed16f15be81b2cff23
Jaachimma.Umeozor <umehandels@gmail.com>
2023-09-29 17:35:42 +0100

    added to cart problem still on it

---
+++ 01 Java-Test/data/product-test.js
@@ -0,0 +1,92 @@
+let products = [
+  {
+    image: "Images/images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating:{
+      stars: 4.5,
+      count: 87
+    },
+    priceCents:1090
+  },
+  {
+    image: "Images/images/products/intermediate-composite-basketball.jpg",
+    name: "Intermediate Size Basketball",
+    rating:{
+      stars: 4,
+      count: 127
+    },
+    priceCents:2095
+  },
+  {
+    image: "Images/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating:{
+      stars: 4.5,
+      count: 56
+    },
+    priceCents:799
+  },
+  {
+    image: "Images/images/products/black-2-slot-toaster.jpg",
+    name: "2 Slot Toaster - Black",
+    rating:{
+      stars: 5,
+      count: 2197
+    },
+    priceCents:1899
+  },
+  {
+    image: "Images/images/products/6-piece-white-dinner-plate-set.jpg",
+    name: "6 Piece White Dinner Plate Set",
+    rating:{
+      stars: 4,
+      count: 37
+    },
+    priceCents:2067
+  },
+  {
+    image: "Images/images/products/6-piece-non-stick-baking-set.webp",
+    name: "6-Piece Nonstick, Carbon Steel Oven Bakeware Baking Set",
+    rating:{
+      stars: 4.5,
+      count: 175
+    },
+    priceCents:3499
+  },
+  {
+    image: "Images/images/products/women-chiffon-beachwear-coverup-black.jpg",
+    name: "Women's Chiffon Beachwear Cover Up - Black",
+    rating:{
+      stars: 4.5,
+      count: 235
+    },
+    priceCents:2070
+  },
+  {
+    image: "Images/images/products/blackout-curtain-set-beige.webp",
+    name: "Blackout Curtains Set 4-Pack - Beige",
+    rating:{
+      stars: 4.5,
+      count: 232
+    },
+    priceCents:4599
+  },
+  {
+    image: "Images/images/products/electric-glass-and-steel-hot-water-kettle.webp",
+    name: "Electric Glass and Steel Hot Tea Water Kettle - 1.7-Liter",
+    rating:{
+      stars: 5,
+      count: 846
+    },
+    priceCents:3074
+  },
+  {
+    image: "Images/images/products/facial-tissue-2-ply-18-boxes.jpg",
+    name: "Ultra Soft Tissue 2-Ply - 18 Box",
+    rating:{
+      stars: 4,
+      count: 99
+    },
+    priceCents:2374
+  }
+]
---
+++ 01 Java-Test/scripts/amazon-test.js
@@ -0,0 +1,77 @@
+let productsHTML ='';
+
+products.forEach((product) => {
+  productsHTML += `
+  <div class="amazon-product-container">
+    <div class="product-image-container">
+      <img  class="product-image" src="${product.image}">
+    </div>
+    <div class="product-name limit-to-2-lines">
+      ${product.name}
+    </div>
+    <div class="rating-stars-container">
+      <img class="rating-stars" src="Images/images/ratings/rating-${product.rating.stars*10}.png">
+      <div class="rating-text">${product.count}</div>
+    </div>
+    <div class="product-price">
+      ${(product.priceCents*10)/1000}
+    </div>
+    <div class="product-select-container">
+      <select>
+        <option selected value="1">1</option>
+        <option value = "2">2</option>
+        <option value = "3">3</option>
+        <option value = "4">4</option>
+        <option value = "5">5</option>
+      </select>
+    </div>
+
+    <p class="added-message js-added-message">
+       
+    <p>
+    
+    <button class="add-to-cart-button js-add-to-cart-button" data-product-name="${product.name}">
+      Add to Cart
+    </button>
+  </div>`;
+});
+
+document.querySelector('.js-amazon-product-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart-button').forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    const productName = button.dataset.productName;
+
+    let matchingItemOne;
+
+    cart.forEach((item)=>{
+      if(productName === item.productName){
+        matchingItemOne = item;
+      }
+    })
+
+    if(matchingItemOne){
+      matchingItemOne.quantity += 1;
+    }else{
+    cart.push({
+      productName : productName,
+      quantity: 1
+    });
+    }
+
+    let cartQuantity = 0;
+
+    cart.forEach((item)=>{
+      cartQuantity += item.quantity;
+      
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    let addedTo = document.querySelector('.js-added-message');
+
+    setTimeout(() => {
+      addedTo.innerHTML = ' <img class="add-to-cart-result-img" src="Images/images/icons/checkmark.png"><div class="add-to-cart-text"> Added</div>'
+    }, 2000);
+  })
+});
